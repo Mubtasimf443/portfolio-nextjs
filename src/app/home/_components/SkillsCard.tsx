b105db70906fac49d1eb3f7a91e84a996e683d79
Mubtasim <mubtasimf443@gmail.com>
2025-04-28 09:50:11 +0600

    SkillsCard Updated ;

--- src/app/home/_components/SkillsCard.tsx
+++ src/app/home/_components/SkillsCard.tsx
@@ -1,13 +1,15 @@
 /* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
 "use client"
+import { LucideIcon } from 'lucide-react';
 import Image from 'next/image';
 import React, { FC, Fragment } from 'react';
 
 interface Props {
-    src: string;
-    alt: string;
+    src?: string;
+    alt?: string;
     name: string;
     className?: string;
+    Icon ?:LucideIcon
 };
 
 const SkillsCard: FC<Props> = (props) => {
@@ -18,17 +20,21 @@ const SkillsCard: FC<Props> = (props) => {
                 border: " 1px solid var(--border)",
                 borderRadius: '0.5rem',
                 padding: 'var(--spacing-lg)',
-
                 textAlign: 'center',
                 transition: 'transform 0.3s, border-color 0.3s'
             }}>
-                <Image
-                    src={props.src}
-                    alt={props.alt}
-                    width={64}
-                    height={64}
-                    className={`skill-card-icon ${props.className ?? ""}`}
-                />
+                {(props.src && props.alt) && (
+                    <Image
+                        src={props.src}
+                        alt={props.alt}
+                        width={64}
+                        height={64}
+                        className={`skill-card-icon ${props.className ?? ""}`}
+                    />
+                )}
+                {
+                    props.Icon && (  <props.Icon />  )
+                }
                 <h4>{props.name}</h4>
             </div>
         </Fragment>
